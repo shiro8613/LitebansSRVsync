ed8a84952c70d420cc9d3d153328b502e7832dcf
toms0910 <psp1070@gmail.com>
2023-02-23 19:13:35 +0900

    Initial commit

---
+++ src/main/java/com/github/shiro8613/litebanssrvsync/Litebanssrvsync.java
@@ -0,0 +1,15 @@
+package com.github.shiro8613.litebanssrvsync;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Litebanssrvsync extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
